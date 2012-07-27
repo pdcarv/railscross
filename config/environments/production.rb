ac8a2fff68e5d29a8b3720b62ff94f06967382c5
Pedro Carvalho <pedro@betabranch.com>
2012-07-27 13:39:37 +0100

    changed heroku initialize on precompile fix to the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,9 @@ Railcross::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  # HEROKU FIX: Do not initialized before heroku database is available 
+  config.assets.initialize_on_precompile = false
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
